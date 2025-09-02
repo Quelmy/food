e483738d5646fb687052a6ff7b0cf7dd5b255176
Quelmy <Ferreirariquelmy27@gmail.com>
2025-09-02 15:43:20 -0300

    Update navbar.jsx
    ajuste

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,38 +1,35 @@
-import "./navbar.css"
+import "./navbar.css";
 import logo from "../../assets/logo.png";
 import bag from "../../assets/bag.png";
 import Cart from "../cart/cart.jsx";
-import {Link} from "react-router-dom";
-function Navbar(props){
+import { Link } from "react-router-dom";
 
-        function openSidebar(){
-            const event = new CustomEvent('openSidebar');
-            window.dispatchEvent(event);
-        }
+function Navbar({ showMenu }) {
 
+    const openSidebar = () => {
+        const event = new CustomEvent('openSidebar');
+        window.dispatchEvent(event);
+    };
 
-    return <div className="navbar">
-        <Link to="/">
-        <img src={logo} className="logotipo" alt="logotipo" />
+    return (
+        <div className="navbar">
+            <Link to="/">
+                <img src={logo} className="logotipo" alt="Logotipo" />
+            </Link>
 
-        </Link>
-        
-        {
-            props.showMenu ?
-
+            {showMenu && (
                 <div className="menu">
-                <Link to="/historico">Historico</Link>
-                <button onClick={openSidebar} className="btn btn-red">
-                    <img src={bag} className="icon" />
-                    Sacola
-                </button>
-            </div>
-        :null
-        }
+                    <Link to="/historico">Histórico</Link>
+                    <button onClick={openSidebar} className="btn btn-red" aria-label="Abrir carrinho">
+                        <img src={bag} className="icon" alt="Ícone da sacola" />
+                        Sacola
+                    </button>
+                </div>
+            )}
 
-        
-        <Cart />
-    </div>
+            <Cart />
+        </div>
+    );
 }
 
 export default Navbar;
